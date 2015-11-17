7d8ec4d303e4f2aac540de92f78d811fde489077
David Milliner <davidmilliner@MacBook-Pro-de-David-2.local>
2015-11-17 19:49:40 +0100

    [WEBSITE] 500 -- favicon new app route

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ import os
 import re
 import urllib
 
-from flask import (Flask, flash, Markup, redirect, render_template, request, Response, session, url_for)
+from flask import (Flask, flash, Markup, redirect, render_template, request, Response, session, url_for, send_from_directory)
 from markdown import markdown
 from markdown.extensions.codehilite import CodeHiliteExtension
 from markdown.extensions.extra import ExtraExtension
@@ -251,6 +251,11 @@ def index():
         query,
         search=search_query,
         check_bounds=False)
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/about')
 def about():
